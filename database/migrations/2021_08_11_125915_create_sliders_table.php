79af006a0efd1348ccbe875684e9db7648ae3615
Kenny Andrian <kennyandrian94@gmail.com>
2021-10-12 14:27:40 +0700

    update db

--- database/migrations/2021_08_11_125915_create_sliders_table.php
+++ database/migrations/2021_08_11_125915_create_sliders_table.php
@@ -16,7 +16,7 @@ class CreateSlidersTable extends Migration
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('location')->unique();
+            $table->string('location', 100)->unique();
             $table->json('settings');
             $table->timestamps();
         });
