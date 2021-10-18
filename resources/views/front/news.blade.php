56e5c6b3371371748270fc94e6468cfdc634994f
Kenny Andrian <kennyandrian94@gmail.com>
2021-10-18 12:20:48 +0700

    update css

--- resources/views/front/news.blade.php
+++ resources/views/front/news.blade.php
@@ -5,8 +5,8 @@
 
 @section('body')
 <section class="bg-gray-200 px-3 lg:mx-auto mt-16 lg:mt-44">
-	<div class="container mx-auto flex justify-between items-center py-8">
-		<div class="text-xl font-bold uppercase tracking-wide">Berita</div>
+	<div class="container mx-auto text-center lg:text-left lg:flex lg:justify-between items-center py-8">
+		<div class="text-xl font-bold uppercase tracking-wide mb-3 lg:mb-0">Berita</div>
 		<div class="text-sm text-gray-500 tracking-wide">
 			<a href="#">Home</a> / <a href="javascript:void(0)" class="font-semibold">Berita</a>
 		</div>
