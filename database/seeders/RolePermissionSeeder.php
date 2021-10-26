faae3c484876556dd2ddde52620123a90a50f306
Fachri Hammad <fachripermadi@gmail.com>
2021-10-26 17:17:34 +0700

    Permission Spatie Added

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Schema::disableForeignKeyConstraints();
+        DB::table('roles')->truncate();
+        DB::table('permissions')->truncate();
+        DB::table('role_has_permissions')->truncate();
+        Schema::enableForeignKeyConstraints();
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $roles = $this->createRoles([
+            'superadmin',
+            'ncp',
+            'ccp',
+            'lccp'
+        ]);
+
+        $masterDataPermissions = $this->createPermissions([
+            'notification',
+            'news',
+            'slider',
+        ]);
+
+        $processPermissions = $this->createPermissions([
+            'downstream',
+            'upstream',
+            'follow_up'
+        ], [
+            'view all',
+            'view',
+            'store',
+            'delete',
+        ]);
+
+        $processPermissions['notification']['process_downstream'] = Permission::create(['name' => 'process_downstream notification']);
+        $processPermissions['notification']['process_upstream'] = Permission::create(['name' => 'process_upstream notification']);
+
+        $processPermissions['upstream']['process_ccp'] = Permission::create(['name' => 'process_ccp upstream']);
+        $processPermissions['upstream']['process_ext'] = Permission::create(['name' => 'process_ext upstream']);
+        $processPermissions['upstream']['finish'] = Permission::create(['name' => 'finish upstream']);
+
+        $processPermissions['downstream']['process_ccp'] = Permission::create(['name' => 'process_ccp downstream']);
+        $processPermissions['downstream']['process_ext'] = Permission::create(['name' => 'process_ext downstream']);
+        $processPermissions['downstream']['finish'] = Permission::create(['name' => 'finish downstream']);
+        
+
+        $processPermissions['follow_up']['process'] = Permission::create(['name' => 'process follow_up']);
+        $processPermissions['follow_up']['accept'] = Permission::create(['name' => 'accept follow_up']);
+        $processPermissions['follow_up']['reject'] = Permission::create(['name' => 'reject follow_up']);
+        
+    }
+
+    public function createPermissions(array $entityList, array $actionList = ['view', 'store', 'delete'])
+    {
+        $permissions = [];
+        foreach ($entityList as $entity) {
+            foreach ($actionList as $action) {
+                $permission = Permission::create(['name' => $action . ' ' . $entity]);
+                $permissions[$entity][$action] = $permission;
+            }
+        }
+
+        return $permissions;
+    }
+
+    public function createRoles(array $roleList)
+    {
+        $roles = [];
+        foreach ($roleList as $role) {
+            $roles[$role] = Role::create(['name' => $role]);
+        }
+
+        return $roles;
+    }
+
+    public function assignEntityPermissions(Role $role, array $permissions, array $entities)
+    {
+        foreach ($entities as $entity) {
+            $role->givePermissionTo($permissions[$entity]);
+        }
+    }
+
+    public function assignEntityActionPermissions(Role $role, array $permissions, array $entityActions)
+    {
+        foreach ($entityActions as $entity => $actions) {
+            foreach ($actions as $action) {
+                $role->givePermissionTo($permissions[$entity][$action]);
+            }
+        }
+    }
+}
